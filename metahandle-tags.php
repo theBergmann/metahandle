41761c4359da11c97c54bc773576033868f4b897
theBergmann <christoph.bergmann@mailbox.org>
2019-08-13 08:21:38 +0200

    Update metahandle-tags.php

--- metahandle-tags.php
+++ metahandle-tags.php
@@ -2,7 +2,7 @@
 <head><meta http-equiv="Content-Type" content="text/html; charset=euc-jp">
     <title>Metahandle - bookmark and tag onchain files onchain</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     <link rel="icon" type="image/png" href="handle-favicon.png">
     <link rel="stylesheet" href="/assets/style.css">
      
